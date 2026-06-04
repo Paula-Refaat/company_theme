f5a528247206dfad246c6491e850b94d242a4005
Paula Refaat <120932892+Paula-Refaat@users.noreply.github.com>
2026-06-04 23:29:41 +0300

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,7 @@
+app_name = "company_theme"
+app_title = "Company Theme"
+app_publisher = "Your Company"
+app_description = "Custom Theme"
+app_version = "0.0.1"
+
+app_include_css = ["assets/company_theme/css/custom.css"]
